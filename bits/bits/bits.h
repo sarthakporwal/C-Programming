e225f6cafc4b78729ccb4b06d2bb98bbe8b066fc
sarthakporwal <sarthakporwal12@gmail.com>
2024-06-17 11:05:15 +0530

    update

---
+++ bits/bits/bits.h
@@ -0,0 +1,18 @@
+//
+//  bits.h
+//  bits
+//
+//  Created by Sarthak Porwal on 05/06/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for bits.
+FOUNDATION_EXPORT double bitsVersionNumber;
+
+//! Project version string for bits.
+FOUNDATION_EXPORT const unsigned char bitsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <bits/PublicHeader.h>
+
+
